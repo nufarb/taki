5cd40ebf61cb96fa3d627cc65cab80a1f3285c5c
nufar braunstein <nufar.br@gmail.com>
2021-05-09 13:38:33 +0300

    update cards file

--- cards.py
+++ cards.py
@@ -1,2 +1,11 @@
 SUPER_TAKI = "super_taki"
+COLOR_CHANGER = 'color_changer'
+KING = 'king'
+PLUS3 = 'plus3'
+BREAK3 = 'break3'
+STOP = 'stop'
+CHANGE_DIRECTION = 'change_direction'
+PLUS = 'plus'
+TAKI = 'taki'
+
 
